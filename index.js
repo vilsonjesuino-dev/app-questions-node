7de51e24dc07fbf275d6444ff2b18b5792084415
Vilson Jesuino | Dev <vilsonjesuino.dev@gmail.com>
2023-05-22 20:14:36 -0300

    --amend

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const questions = []
+// this is a questions today
+const questions = [
+  "O que aprendi hoje?",
+  "O que me deixou aborrecido?",
+  "O que eu poderia fazer para melhorar?",
+  "O que me deixou feliz hoje?",
+  "Quantas pessoas ajudei hoje?"
+]
+
+const ask = (index = 0) => {
+  process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+ask()
+
